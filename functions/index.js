38b7688d0ee2c1217d918c806fc7ffab7888ddbd
CardSorting <willcruzdesigner@gmail.com>
2026-01-15 21:00:13 -0700

    inde

--- functions/index.js
+++ functions/index.js
@@ -3,6 +3,8 @@ import "./firebaseInit.js"; // Ensure Firebase Admin is initialized
 
 import { api } from "./api.js";
 import { web } from "./web.js";
+import { urgentWorker, backgroundWorker } from "./workers/queues.js";
+
 // Export the main services
 export {
     api,
