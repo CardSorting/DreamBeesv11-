fd79c1e7513b1ad4de4443ed662fd59b98700581
CardSorting <willcruzdesigner@gmail.com>
2026-01-17 15:46:58 -0700

    feat: Update Firebase hosting cache with new showcase model images.

--- functions/index.js
+++ functions/index.js
@@ -2,13 +2,14 @@
 import "./firebaseInit.js"; // Ensure Firebase Admin is initialized
 
 import { api } from "./api.js";
-import { web } from "./web.js";
+import { web, serveSitemap } from "./web.js";
 import { urgentWorker, backgroundWorker } from "./workers/queues.js";
 
 // Export the main services
 export {
     api,
     web,
+    serveSitemap,
     urgentWorker,
     backgroundWorker,
 };
